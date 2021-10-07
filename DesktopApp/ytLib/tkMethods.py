5a13462f29902edfc1f6b4b8a648354d4548e738
Diego MS <diego.mtzs00@gmail.com>
2021-10-07 11:17:44 -0500

    Update tkMethods.py
    Added wraplength flag in order to show better biggest titles and descriptions

--- DesktopApp/ytLib/tkMethods.py
+++ DesktopApp/ytLib/tkMethods.py
@@ -278,9 +278,9 @@ class tkClass(extraMethods):
         ImaLabel.place(x= 20, y= 320)
 
         # Video info
-        thumbTitleLabel= tk.Label(raiz, text= "", font= ("jost", 10))
+        thumbTitleLabel= tk.Label(raiz, text= "", font= ("jost", 10), wraplength= 300)
         thumbAuthorLabel= tk.Label(raiz, text= "", font= ("jost", 10))
-        thumbDescLabel= tk.Label(raiz, text= "", font= ("jost", 10))
+        thumbDescLabel= tk.Label(raiz, text= "", font= ("jost", 10), wraplength= 300)
         
         thumbTitleLabel.place(x= 225, y=330)
         thumbAuthorLabel.place(x= 225, y= 360)
